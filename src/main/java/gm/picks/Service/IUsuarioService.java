44999c7dc2544e092ef2ef4b730a07e379a04278
iAndrest-T <atabares399@gmail.com>
2025-04-09 22:22:31 -0500

    Modificar el método findUsuario para buscar por ID en lugar de nombre de usuario

--- src/main/java/gm/picks/Service/IUsuarioService.java
+++ src/main/java/gm/picks/Service/IUsuarioService.java
@@ -1,12 +1,15 @@
 package gm.picks.Service;
 
+import gm.picks.Models.Pick;
 import gm.picks.Models.Usuario;
 
 public interface IUsuarioService {
 
-    public Usuario findUsuario(String username);
+    public Usuario findUsuario(int idUser);
 
     public boolean validateLogin(Usuario usuario);
+
+    public void updateSaldo(Pick pick);
 
     public void addUsuario(Usuario usuario);
 }
--- src/main/java/gm/picks/Service/UsuarioService.java
+++ src/main/java/gm/picks/Service/UsuarioService.java
@@ -1,5 +1,6 @@
 package gm.picks.Service;
 
+import gm.picks.Models.Pick;
 import gm.picks.Models.Usuario;
 import gm.picks.Repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,17 +13,24 @@ public class UsuarioService implements IUsuarioService {
     private UsuarioRepository usuarioRepository;
 
     @Override
-    public Usuario findUsuario(String username) {
-        return usuarioRepository.findAll()
-                .stream()
-                .filter(u -> u.getUserName().equals(username))
-                .findFirst()
-                .orElse(null);
+    public Usuario findUsuario(int idUser) {
+        return usuarioRepository.findById(idUser).orElse(null);
     }
 
     @Override
     public boolean validateLogin(Usuario usuario) {
         return false;
+    }
+
+    @Override
+    public void updateSaldo(Pick pick) {
+        Usuario usuario = findUsuario(pick.getUsuario().getId());
+        if (pick.getResultado().equals("Acierto")) {
+            usuario.setSaldoActual(usuario.getSaldoActual() + pick.getUtilidadPick());
+        } else if (pick.getResultado().equals("Perdido")) {
+            usuario.setSaldoActual(usuario.getSaldoActual() - pick.getValor());
+        }
+        addUsuario(usuario);
     }
 
     @Override
