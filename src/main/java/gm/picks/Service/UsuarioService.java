1cfaf7a7e32f39b8931bcb93d50b8f8b8bec3c9c
iAndrest-T <atabares399@gmail.com>
2025-04-10 22:29:01 -0500

    Modificar el método updateSaldo en UsuarioService para aceptar un ID de usuario y actualizar el saldo basado en los rendimientos obtenidos.

--- src/main/java/gm/picks/Service/UsuarioService.java
+++ src/main/java/gm/picks/Service/UsuarioService.java
@@ -1,6 +1,5 @@
 package gm.picks.Service;
 
-import gm.picks.Models.Pick;
 import gm.picks.Models.Usuario;
 import gm.picks.Repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,14 +22,17 @@ public class UsuarioService implements IUsuarioService {
     }
 
     @Override
-    public void updateSaldo(Pick pick) {
-        Usuario usuario = findUsuario(pick.getUsuario().getId());
-        if (pick.getResultado().equals("Acierto")) {
-            usuario.setSaldoActual(usuario.getSaldoActual() + pick.getUtilidadPick());
-        } else if (pick.getResultado().equals("Perdido")) {
-            usuario.setSaldoActual(usuario.getSaldoActual() - pick.getValor());
-        }
+    public void updateSaldo(int idUser) {
+        Usuario usuario = findUsuario(idUser);
+        Object[] rendimientos = getRendimientos(usuario.getId(), 0, "");
+        usuario.setSaldoActual((Double) rendimientos[1]);
         addUsuario(usuario);
+    }
+
+    @Override
+    public Object[] getRendimientos(int userId, Integer sportId, String mes) {
+        Object[] result = (Object[]) usuarioRepository.getRendimientos(userId, sportId, mes);
+        return (result != null && result.length >= 2) ? result : null;
     }
 
     @Override
