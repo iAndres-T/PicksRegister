c8f9e12c17eb475bec2dfcd07777f1b05f04a7ae
iAndrest-T <atabares399@gmail.com>
2025-04-10 22:28:41 -0500

    Agregar método getRendimientos en UsuarioRepository para ejecutar el procedimiento almacenado sp_GetRendimientos

--- src/main/java/gm/picks/Repository/UsuarioRepository.java
+++ src/main/java/gm/picks/Repository/UsuarioRepository.java
@@ -2,6 +2,20 @@ package gm.picks.Repository;
 
 import gm.picks.Models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+  @Query(value = """
+        EXEC sp_GetRendimientos 
+            @UserId = :userId, 
+            @SportId = :sportId, 
+            @Mes = :mes
+        """, nativeQuery = true)
+    Object getRendimientos(
+        @Param("userId") Integer userId, 
+        @Param("sportId") Integer sportId, 
+        @Param("mes") String mes
+    );
 }
