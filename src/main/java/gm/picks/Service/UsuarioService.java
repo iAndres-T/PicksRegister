a91870fb6d591b8bead67f45074c137b25ae0bd4
iAndrest-T <atabares399@gmail.com>
2025-04-27 22:22:06 -0500

    Refactor lógica de validación de usuario y encriptación de contraseña en UsuarioService

--- src/main/java/gm/picks/Service/UsuarioService.java
+++ src/main/java/gm/picks/Service/UsuarioService.java
@@ -4,6 +4,7 @@ import gm.picks.Models.Usuario;
 import gm.picks.Repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Service
 public class UsuarioService implements IUsuarioService {
@@ -18,6 +19,15 @@ public class UsuarioService implements IUsuarioService {
 
     @Override
     public boolean validateLogin(Usuario usuario) {
+        Usuario user = usuarioRepository.findAll()
+                .stream()
+                .filter(u -> u.getUserName().equals(usuario.getUserName()))
+                .findFirst()
+                .orElse(null);
+        if (user != null) {
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            return encoder.matches(usuario.getPassword(), user.getPassword());
+        }
         return false;
     }
 
@@ -37,6 +47,8 @@ public class UsuarioService implements IUsuarioService {
 
     @Override
     public void addUsuario(Usuario usuario) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        usuario.setPassword(encoder.encode(usuario.getPassword()));
         usuarioRepository.save(usuario);
     }
 }
