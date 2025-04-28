2f8deebae79e2d19e1933c8b7484eb5b91ee5933
iAndrest-T <atabares399@gmail.com>
2025-04-27 22:21:50 -0500

    Agregar controlador de inicio de sesión con lógica de validación de usuario

---
+++ src/main/java/gm/picks/Controllers/LoginController.java
@@ -0,0 +1,45 @@
+package gm.picks.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import gm.picks.Models.Usuario;
+import gm.picks.Service.UsuarioService;
+
+@Controller
+public class LoginController {
+  private ObjectMapper objectMapper = new ObjectMapper();
+
+  @Autowired
+  UsuarioService usuarioService;
+
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String loginPage(ModelMap modelo) {
+    modelo.put("Script", "Login.js");
+    return "Login";
+  }
+
+  @RequestMapping(value = "/login", method = RequestMethod.POST)
+  @ResponseBody
+  public String login(@RequestBody String usuarioModel) throws JsonProcessingException {
+    try {
+      Usuario usuario = objectMapper.readValue(usuarioModel, Usuario.class);
+      boolean validate = usuarioService.validateLogin(usuario);
+      if (validate) {
+        return "redirect:/picks/Home";
+      } else {
+        return "Invalid";
+      }
+    } catch (Exception e) {
+      return "Error: " + e.getMessage();
+    }
+  }
+}
