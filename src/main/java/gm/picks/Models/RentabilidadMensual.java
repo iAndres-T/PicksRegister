f429b93ddb07518f9a637d7e5cc7e46e01e75f99
iAndrest-T <atabares399@gmail.com>
2025-05-01 22:45:23 -0500

    Agregar la clase RentabilidadMensual con atributos para modelar la rentabilidad mensual de los usuarios

--- src/main/java/gm/picks/Models/RentabilidadMensual.java
+++ src/main/java/gm/picks/Models/RentabilidadMensual.java
@@ -1,4 +1,38 @@
 package gm.picks.Models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class RentabilidadMensual {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", referencedColumnName = "id", nullable = false)
+    Usuario usuario;
+
+    @Column(nullable = false)
+    Double ganancia;
+
+    @Column(nullable = false)
+    Double porcentaje;
+
+    @Column(nullable = false)
+    Double saldoMes;
+
+    @Column(nullable = false)
+    String mes;
+
+    @Column(nullable = false)
+    Integer anio;
 }
