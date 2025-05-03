7e9d7476a81da5a40eff3422285312edb2c67526
iAndrest-T <atabares399@gmail.com>
2025-05-02 22:05:08 -0500

    Agregar lógica para manejar la actualización de capital en caso de cambio de mes: mostrar modal de confirmación y permitir modificar o mantener el capital.

--- src/main/webapp/resources/js/Login.js
+++ src/main/webapp/resources/js/Login.js
@@ -1,7 +1,9 @@
+const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+let URL;
+
 $("#btnLogin").click(function () {
   const inputs = $("input.input-validar").serializeArray();
   const inputs_sin_valor = inputs.filter((item) => item.value.trim() == "");
-  let URL;
 
   if (inputs_sin_valor.length > 0) {
     const mensaje = `Debe completar el campo`;
@@ -28,7 +30,20 @@ $("#btnLogin").click(function () {
       }
       else if (text.endsWith("UpdateMes")) {
         URL = text.split("-")[0].replace("redirect:", "");
-        window.location.href = URL;
+        Swal.fire({
+          title: "Actualizar Capital",
+          text: "El mes ha cambiado, ¿Desea cambiar su Capital?",
+          icon: "question",
+          showDenyButton: true,
+          confirmButtonText: "Modificar mi capital",
+          denyButtonText: "Mantener mi capital",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            $("#modalBank").modal("show");
+          } else if(result.isDenied) {
+            actualizarBank(null);
+          }
+        });
       }
       else if (text.startsWith("redirect:")) {
         URL = text.replace("redirect:", "");
@@ -39,3 +54,42 @@ $("#btnLogin").click(function () {
       toastr.error("", "Error al validar el usuario" + error);
     });
 })
+
+$("#btnActualizarBank").click(function () {
+
+  if ($("#txtNuevoCapital").val() == '') {
+    toastr.warning("", `Debe completar el campo`);
+    $(`#txtNuevoCapital`).focus();
+    return;
+  }
+
+  actualizarBank($("#txtNuevoCapital").val());
+});
+
+async function actualizarBank(bank) {
+  let data = {};
+  data["userName"] = $("#registerUsername").val();
+  data["capital"] = bank;
+  data["mes"] = monthNames[new Date().getMonth()];
+  data["anio"] = new Date().getFullYear();
+
+  fetch("/picks/actualizarBank", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then(async (response) => {
+      if (response.ok) {
+        window.location.href = URL;
+      }
+      else {
+        toastr.error("", "Error al actualizar el capital" + text);
+        window.location.reload();
+      }
+    })
+    .catch((error) => {
+      toastr.error("", "Error al validar el usuario" + error);
+    });
+}
