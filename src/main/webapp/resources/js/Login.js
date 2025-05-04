210eda1aeaee4ecb6fbea88d8224bf3abf1b7668
iAndrest-T <atabares399@gmail.com>
2025-05-04 16:25:02 -0500

    Actualizar la validación en el registro de usuario y la actualización de capital, mejorando la gestión de errores y mensajes de advertencia.

--- src/main/webapp/resources/js/Login.js
+++ src/main/webapp/resources/js/Login.js
@@ -2,7 +2,7 @@ const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Juli
 let URL;
 
 $("#btnLogin").click(function () {
-  const inputs = $("input.input-validar").serializeArray();
+  const inputs = $("input.login-validar").serializeArray();
   const inputs_sin_valor = inputs.filter((item) => item.value.trim() == "");
 
   if (inputs_sin_valor.length > 0) {
@@ -81,15 +81,79 @@ async function actualizarBank(bank) {
     body: JSON.stringify(data),
   })
     .then(async (response) => {
-      if (response.ok) {
+      const text = await response.text();
+      if (text === "true") {
         window.location.href = URL;
+      }
+      else if (text === "Same") {
+        toastr.warning("", "El capital es el mismo que el mes anterior");
       }
       else {
         toastr.error("", "Error al actualizar el capital" + text);
-        window.location.reload();
+        setTimeout(window.location.reload(), 2000);
       }
     })
     .catch((error) => {
       toastr.error("", "Error al validar el usuario" + error);
     });
 }
+
+$("#btnRegisterUser").click(function () {
+  const inputs = $("input.register-validar").serializeArray();
+  const inputs_sin_valor = inputs.filter((item) => item.value.trim() == "");
+
+  if (inputs_sin_valor.length > 0) {
+    const mensaje = `Debe completar el campo`;
+    toastr.warning("", mensaje);
+    $(`input[name="${inputs_sin_valor[0].name}"]`).focus();
+    return;
+  }
+
+  if ($("#registerPassword").val() !== $("#confirmPassword").val()) {
+    toastr.warning("", "Las contraseñas no coinciden");
+    $("#registerPassword").focus();
+    return;
+  }
+
+  let user = {};
+  user["userName"] = $("#registerUserName").val();
+  user["password"] = $("#registerPassword").val();
+  user["capitalInvertido"] = $("#registerCapital").val();
+  user["saldoActual"] = $("#registerCapital").val();
+  user["saldoInicialMes"] = $("#registerCapital").val();
+  user["saldoInicialHistory"] = $("#registerCapital").val();
+  user["mesActual"] = monthNames[new Date().getMonth()];
+  user["rol"] = {id: 1};
+
+  fetch("/picks/registerUser", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then(async (response) => {
+      const text = await response.text();
+      if (text === "Exists") {
+        toastr.warning("", "El UserName ya existe");
+      }
+      else if (text === "OK") {
+        Swal.fire({
+          title: "Usuario registrado",
+          text: "El usuario ha sido registrado correctamente",
+          icon: "success",
+          timer: 2000,
+          showConfirmButton: false,
+        });
+        setTimeout(() => {
+          window.location.reload();
+        }, 4000);
+      }
+      else {
+        toastr.error("", "Error al registrar el usuario");
+      }
+    })
+    .catch((error) => {
+      toastr.error("", "Error al validar el usuario");
+    });
+});
